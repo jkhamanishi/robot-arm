ac7daeeeacd86e43dec9b33cf391f03326d093e6
Jackson Hamanishi <jkhamanishi@gmail.com>
2023-12-07 19:27:12 -0800

    Turned off debug mode

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from controller import ControllerApp
 if __name__ == "__main__":
     # Establish connection
     arm = usb_arm.Arm()
+    arm.blink(2)
 
     # Connect to controller
     app = ControllerApp(arm)
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
+    app.run(host="0.0.0.0", port=5000, debug=False)
